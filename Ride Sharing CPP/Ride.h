7fa49c4c2a1fa223abf888cc23c85438745e9898
Priya <168474848+priyathapa11@users.noreply.github.com>
2026-07-06 13:36:01 -0500

    Update Ride.h

--- Ride Sharing CPP/Ride.h
+++ Ride Sharing CPP/Ride.h
@@ -1,1 +1,30 @@
 
+#ifndef RIDE_H
+#define RIDE_H
+
+#include <string>
+using namespace std;
+
+class Ride {
+private:
+    int rideID;
+    string pickupLocation;
+    string dropoffLocation;
+    double distance;
+    double fare;
+
+public:
+    Ride(int id, string pickup, string dropoff, double dist);
+    virtual ~Ride();
+
+    int getRideID() const;
+    double getDistance() const;
+
+    void setFare(double fare);
+    double getFare() const;
+
+    virtual double calculateFare();
+    virtual void rideDetails();
+};
+
+#endif
